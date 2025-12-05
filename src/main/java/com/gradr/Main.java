da7957d33568412c02afbfeb86d46999580f3d25
Nana Kwaku <nksarps@gmail.com>
2025-12-05 10:42:26 +0000

    added new features to the main menu

--- src/main/java/com/gradr/Main.java
+++ src/main/java/com/gradr/Main.java
@@ -366,7 +366,12 @@ public class Main {
         System.out.println("2. View Students");
         System.out.println("3. Record Grade");
         System.out.println("4. View Grade Report");
-        System.out.println("5. Exit");
+        System.out.println("5. Export Grade Report [NEW]");
+        System.out.println("6. Calculate Student GPA [NEW]");
+        System.out.println("7. Bulk Import Grades [NEW]");
+        System.out.println("8. View Class Statistics [NEW]");
+        System.out.println("9. Search Students [NEW]");
+        System.out.println("10. Exit");
         System.out.println();
     }
 
