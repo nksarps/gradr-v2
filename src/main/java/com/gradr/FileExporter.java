e6b914236c855b1cb32a35614f39e1746afb5e95
Nana Kwaku <nksarps@gmail.com>
2025-12-11 00:02:23 +0000

    added comments to help recall the logic behind the code

--- src/main/java/com/gradr/FileExporter.java
+++ src/main/java/com/gradr/FileExporter.java
@@ -10,25 +10,28 @@ import java.nio.file.Paths;
 
 
 public class FileExporter {
+    // The location of the file to be exported eg. ./reports/fileName.txt
     private String fileLocation;
 
     /**
      * Constructor for the FileExporter
-     * @param fileName
+     * @param fileName - The name of the file to be exported eg. "report1"
+     * @throws FileExportException - If the file cannot be created
      */
     public FileExporter(String fileName) throws FileExportException {
         this.fileLocation = String.format("./reports/%s.txt", fileName);
 
+        // Creating the directory to store the exported files if it doesn't exist
         try {
             Files.createDirectories(Paths.get("./reports"));
         } catch (IOException e) {
-            throw new FileExportException("Failed to create reports directory");
+            throw new FileExportException("X ERROR: FileExportException\n   Failed to create reports directory");
         }
     }
 
     /**
      * Method that exports student's grades to .txt files
-     * @param content
+     * @param content - The content of the file to be exported  
      * @throws FileExportException
      */
     public void exportGradeToTXT(String content) throws FileExportException {
@@ -37,7 +40,7 @@ public class FileExporter {
             writer.write(content);
             writer.close();
         } catch (IOException e) {
-            throw new FileExportException("Failed to export grades to file: " + fileLocation);
+            throw new FileExportException("X ERROR: FileExportException\n   Failed to export grades to file: " + fileLocation);
         }
 
     }
